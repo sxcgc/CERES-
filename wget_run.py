f9e253156d8ce5826c4f831f5305883ca1155492
ustccgc <44256476+ustccgc@users.noreply.github.com>
2019-03-29 10:57:49 +0800

    chang_something

--- wget_run.py
+++ wget_run.py
@@ -1,10 +1,14 @@
 import yaml
 import datetime
 import os
-first_path = os.getcwd()
+#first_path = os.getcwd()
 
+device = 4
 
-
+f_sh ={}
+for i in range(4):
+    f_sh[str(i)] = open('wget_input_'+srt(i)+'.sh','w')
+	
 data_email = yaml.load(open('track_change.yaml'))
 data_message = yaml.load(open('message.yaml'))
 
@@ -17,30 +21,41 @@ for keyword in path_all.keys():
             print(keyword+'/'+dirfile +' was created')
 
 #print(data_message)
-f_sh = open('wget_input.sh','w')
 f_error = open('download_aqua_error','w')
+flag = 0
 timedelta = datetime.timedelta(days=1)
-dmin = datetime.datetime.now()
-for dkey in data_message:
-    print(dkey)
-    f_error.writelines(dkey + '\r\n')
-    for d in data_message[dkey]:
-        ddate = datetime.datetime.strptime(d[:], '%Y-%m-%d')
-        flag = 1
-        for key in data_email.keys():
-            email = data_email[key]
+index = 0
+
+for key in data_email.keys():
+    email = data_email[key]
+    for dkey in data_message:
+        for d in data_message[dkey]:
+            ddate = datetime.datetime.strptime(d[:],'%Y-%m-%d')
             for e in email:
-                edate = datetime.datetime.strptime(e[40:50],'%Y%m%d%H%M')
-                if (dmin > edate):
-                    dmin = edate
-                if(ddate+timedelta > edate and ddate-timedelta < edate):
-                    flag = 0
-                    f_sh.writelines('axel -n 10 -o ./'+ dkey+'/' + d + '/'+ '  ftp://xfr140.larc.nasa.gov/'+key+'/'+e[0:50]+' \n')
-        if (flag == 1):
-            f_error.writelines(d + ' no\r\n')
-        else:
-            f_error.writelines(d + ' yes\r\n')
-print(dmin)
+                edate = datetime.datetime.strptime(e[40:48],'%Y%m%d')
+                if(ddate == edate):
+                    flag = 1
+					index = index +1
+            if(flag == 1):
+                flag = 0
+                f_error.writelines(d + ' no\r\n')
+
+for i in range(device):
+    for j in range(index/device):
+        f_sh[srt(i)].writelines('wget ./'+ dkey+'/' + d + '/'+ ' ftp://xfr140.larc.nasa.gov/'+key+'/'+e[0:50]+'\r\n')
+
+for i in range(device):
+    f_sh[str(i)].close()
+#f_sh.writelines('wget ./'+ dkey+'/' + d + '/'+ ' ftp://xfr140.larc.nasa.gov/'+key+'/'+e[0:50]+'\r\n')
 f_error.close()
-f_sh.close()
+#f_sh.close()
+f = open('run.sh','w')
+
+for i in range(device):
+    f.writelines('chmod 701 wget_input_*.sh')
+
+f.close()
+
+
+
         
